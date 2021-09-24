c62b2695f12dbd8443dc5de8d8f69c4a15e65623
Pedro <krazyabyss@gmail.com>
2021-09-24 12:46:03 +0200

    Hola Mundo en VB y Windows Forms

---
+++ HelloWorld_WPF_WindowsForms/HelloWorld_WindosForms_VB/HolaMundo.vb
@@ -0,0 +1,15 @@
+﻿Public Class HolaMundo
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Hola " + txtNombre.Text)
+
+    End Sub
+
+    Private Sub HolaMundo_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
